c1568a404f194ba9351f6f99da57208198ba834a
GuilhermeW3ber <brococaxi@hotmai.com>
2023-02-26 04:29:05 -0300

    Third Commit

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+//loader
 window.addEventListener("load", ()=>{
     const loader=document.querySelector(".loader")
     loader.classList.add("loader-hidden")
@@ -6,3 +7,26 @@ window.addEventListener("load", ()=>{
         documento.body.removeChild("loader")
     })
 })
+
+//menu mobile
+const menu = document.querySelector('#mobile-menu');
+const menuPages = document.querySelector('.navbarMenu');
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuPages.classList.toggle('active');
+}
+
+menu.addEventListener('click', mobileMenu);
+
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
